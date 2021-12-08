43562ba174c9e617350167c72c364177851f8d49
Alessandro Canevaro <s212977@dtu.dk>
2021-12-08 12:09:21 +0100

    airport outline

--- comp/Airport.py
+++ comp/Airport.py
@@ -1,12 +1,46 @@
 
 class Airport:
-	pass
+	def __init__(self) -> None:
+		self.ICAO = ''
+		self.city = ''
+		self.country = ''
+		self.latitude = ''
+		self.longitude = ''
+
+		self.terminals = []
+		self.runways = []
+
+		self.destinations = []
+
+	def __eq__(self, o: object) -> bool:
+		pass
+
+	def __repr__(self) -> str:
+		pass
+
+	def update(self) -> None:
+		pass
+
 
 class Terminal:
-	pass
+	def __init__(self, ICAO, ID) -> None:
+		self.ICAO = ICAO
+		self.id = ID
+
+		self.gates = []
+		self.capacity = 0
+
 
 class Gate:
-	pass
+	def __init__(self, terminal_id) -> None:
+		self.terminal_id = terminal_id
+		self.id = ''
+		self.status = 'empty'
+
 
 class Runway:
-	pass
+	def __init__(self, ICAO, ID) -> None:
+		self.ICAO = ICAO
+		self.id = ID
+
+		self.status = 'empty'
