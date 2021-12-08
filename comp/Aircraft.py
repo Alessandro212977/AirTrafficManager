8568877407a8f27d940218d55ff723989c5c7b6e
Alessandro Canevaro <s212977@dtu.dk>
2021-12-08 12:14:41 +0100

    airplane outline

--- comp/Aircraft.py
+++ comp/Aircraft.py
@@ -1,3 +1,18 @@
 
 class Aircraft:
-	pass
+	def __init__(self) -> None:
+		self.id = 0
+		self.type = ''
+		self.route = ''
+		self.capacity = ''
+		self.status = 'landing'
+		self.destination = ''
+
+	def board(self):
+		pass
+
+	def landing(self):
+		pass
+
+	def update(self):
+		pass
